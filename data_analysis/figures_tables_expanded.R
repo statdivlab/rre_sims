3a8fe5f85853494cac91c7b0547166aa26a9c1c2
AlexPaynter <alexcpaynter@gmail.com>
2019-08-25 18:27:25 -0700

    Manuscript data analysis first draft

--- data_analysis/figures_tables_expanded.R
+++ data_analysis/figures_tables_expanded.R
@@ -2,12 +2,6 @@
 
 load("data_analysis_expanded_results.RData")
 
-sample_outputs$sample_1 %>% lapply(., (function(x) c(x$ccc, x$selected_lambda, x$alpha, x$delta))) %>%
-  do.call(rbind, .) %>%
-  'colnames<-'(., c("ccc", "selected_lambda", "alpha", "delta")) %>%
-  as.data.frame(.) %>%
-  tibble::add_column(method = 0:4)
-
 one_sample <- function(sample) {
   lapply(sample, (function(x) c(x$ccc, x$selected_lambda, x$alpha, x$delta))) %>%
     do.call(rbind, .) %>%
@@ -41,7 +35,7 @@ plot_fn <- function(output_df,
   
   if (any(output_df$method_display == "error")) stop("Coding not recognized")
   
-  pd <- position_dodge(0.1)
+  pd <- position_dodge(0)
   gg <- ggplot(output_df, aes(x = year, y = ccc, 
                               group = method_display, 
                               color = method_display)) +
@@ -67,6 +61,88 @@ plot_fn <- function(output_df,
   }
 }
 
+sample_outputs %>% all_samples %>% str
 
-all_samples(sample_outputs) %>% plot_fn(.)
+# compact table output:
+table_to_console_1 <- function(outs) {
+  outs %<>% all_samples  
+  outs %<>% dplyr::mutate(method = dplyr::case_when(
+    method == 0 ~ "{[0]} Unpenalized MLE", # method 00
+    method == 1 ~ "{[1]} Minimum Variance", # method 01
+    method == 2 ~ "{[2]} CV Likelihood", # method 02
+    method == 3 ~ "{[3]} Goodness of fit", # method 03
+    method == 4 ~ "{[4]} CV G.O.F.", # method 04 
+    TRUE ~ "error")) %>%
+    dplyr::mutate(method = factor(method))
+  
+  if (any(outs$method_display == "error")) stop("Coding not recognized")
+  
+  outs %<>% dplyr::mutate(ccc = formatC(ccc, format="d", big.mark=","),
+                         selected_lambda = ifelse(is.na(selected_lambda), 
+                                                  "\\textemdash", 
+                                                  as.character(selected_lambda)),
+                         alpha = formatC(alpha, format = "f", digits = 5),
+                         delta = formatC(delta, format = "f", digits = 5))
+  # This is rearrangement for latex output:
+  cols_1 <- outs[1:5,2:6]
+  cols_2 <- outs[6:10,3:6]
+  cols_3 <- outs[11:15,3:6]
+  flat_table <- cbind(cols_1,cols_2,cols_3)
+  
+  ft_names <- c("$\\widehat{C}$", "$\\widetilde{\\lambda}$",
+                "$\\widehat{\\alpha}$", "$\\widehat{\\delta}$")
+  colnames(flat_table) <- c("Method", rep(ft_names, times = 3))
+  flat_table %>% xtable::xtable(.) %>% print(., 
+                                          sanitize.text.function = identity,
+                                          sanitize.rownames.function = identity,
+                                          santitize.colnames.function = identity,
+                                          include.rownames = F,
+                                          booktabs = T)
+  # after this we manually insert multicolumn row for years: 2009, 2010 and 2011.
+}
+
+# condensed table output:
+table_to_console_2 <- function(outs) {
+  outs %<>% all_samples  
+  outs %<>% dplyr::mutate(method = dplyr::case_when(
+    method == 0 ~ "{[0]} Unpenalized MLE", # method 00
+    method == 1 ~ "{[1]} Minimum Variance", # method 01
+    method == 2 ~ "{[2]} CV Likelihood", # method 02
+    method == 3 ~ "{[3]} Goodness of fit", # method 03
+    method == 4 ~ "{[4]} CV G.O.F.", # method 04 
+    TRUE ~ "error")) %>%
+    dplyr::mutate(method = factor(method))
+  
+  if (any(outs$method_display == "error")) stop("Coding not recognized")
+  
+  outs %<>% dplyr::mutate(ccc = formatC(ccc, format="d", big.mark=","),
+                          selected_lambda = ifelse(is.na(selected_lambda), 
+                                                   "\\textemdash", 
+                                                   as.character(selected_lambda)),
+                          alpha = formatC(alpha, format = "f", digits = 5),
+                          delta = formatC(delta, format = "f", digits = 5))
+  # This is rearrangement for latex output:
+  cols_1 <- outs[1:5,2:6]
+  cols_2 <- outs[6:10,3:6]
+  cols_3 <- outs[11:15,3:6]
+  flat_table <- cbind(cols_1,cols_2,cols_3)
+  
+  ft_names <- c("$\\widehat{C}$", "$\\widetilde{\\lambda}$",
+                "$\\widehat{\\alpha}$", "$\\widehat{\\delta}$")
+  colnames(flat_table) <- c("Method", rep(ft_names, times = 3))
+  flat_table %>% xtable::xtable(.) %>% print(., 
+                                             sanitize.text.function = identity,
+                                             sanitize.rownames.function = identity,
+                                             santitize.colnames.function = identity,
+                                             include.rownames = F,
+                                             booktabs = T)
+  # after this we manually insert multicolumn row for years: 2009, 2010 and 2011.
+}
+
+sample_outputs %>% table_to_console_1
+sample_outputs %>% table_to_console_2
+
+sample_outputs %>% all_samples %>% plot_fn(.)
+sample_outputs %>% all_samples %>% plot_fn(., "../rre_manuscript/images/data_analysis_expanded.pdf")
+
 
